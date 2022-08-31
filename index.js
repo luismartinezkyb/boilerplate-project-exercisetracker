c022a4dce6460a5f2808961e08857a08e1714906
Luis Martinez <18030545@itcelaya.edu.mx>
2022-08-30 21:31:37 -0500

    first version

--- index.js
+++ index.js
@@ -1,18 +1,160 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const mySecret = process.env.MONGODB_URI
 
+//bd connection and schemas
+//mongodb+srv://luismartinez:2k74lfZJQ0YxvXQN@cluster0.wssr2.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true});
+
+//bd schema and model
+const userSchema = mongoose.Schema({
+  username:             {type: String, required: true}
+}, { versionKey: false });
+
+const Users= mongoose.model('Users', userSchema);
+
+const exerciseSchema = mongoose.Schema({
+  description:           {type: String, required: true},
+  duration:              {type: Number, required: true},
+  date:                  {type: String, required: true},
+  date2:                  {type: Date, required: true},
+  userId:   {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'Users',
+    required: true
+  },
+}, { versionKey: false });
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+
+//middlewares
 app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static('public'))
+
+
+//routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get('/api/users', (req, res)=>{
+  Users.find({}, (err, data)=>{
+    if(err) return console.error(err);
+    data && data.length!==0  ? res.send(data) : res.send({"error": "No hay users"});
+  });
+})
+
+app.post('/api/users', (req, res)=>{
+  const {username} = req.body;
+  console.log(username)
+  
+  const newUser = new Users({
+    username: username
+  })
+  newUser.save((err, data)=>{
+    if(err){
+      return res.status(400).send(err)
+    } 
+    res.send(data);
+  })
+  
+});
+
+app.post('/api/users/:id/exercises', (req, res)=>{
+  const {id} = req.params;
+  var date = "";
+  if(req.body['date'].length === 0){
+    date = new Date();    
+  }
+  else{
+     
+    date = req.body['date'];
+  }
+  const {description, duration} =  req.body; 
+  var checkDate2 = new Date(date);
+  var checkDate = new Date(date).toDateString();
+  Users.findById(id, (err, data)=>{
+    if(err){
+      return res.status(400).send(err.message)
+    }
+    const name = data.username;
+    const newExercise = new Exercise({
+      date: checkDate,
+      date2: checkDate2,
+      duration: duration,
+      description: description,
+      userId: id
+    });
+    newExercise.save((err, data)=>{
+      if(err){
+        return res.status(400).send(err.message)
+      }
+      res.send({
+      "_id": id,
+      "username": name,
+      "date": data.date,
+      "duration": data.duration,
+      "description": data.description
+      });
+    });
+    
+  });
+});
+
+app.get('/api/users/:id/logs', async(req, res)=>{
+  const {id} = req.params;
+  var from = new Date('2000-00-00');
+  var to;
+  var limit = 0;
+  
+  
+  try {
+    const user = await Users.findById(id);
+    if (user.length === 0) {
+      return res.status(400).send('there is no users')
+    }else{
+      var exerciseFind = await Exercise.find({userId : id}, { description: 1, duration: 1, date: 1, _id: 0 });
+      // var newArr = [...exerciseFind];
+      if(exerciseFind){
+        var newArr = exerciseFind.slice(0);
+        if(req.query['from']){
+          from = new Date(req.query['from'])
+          newArr= newArr.filter(e => new Date(e.date)>=from);
+        }
+        if(req.query['to']){
+          to = new Date(req.query['to'])
+          newArr = newArr.filter(e => new Date(e.date)<=to);
+        }
+        if(req.query['limit']){
+          limit = Number(req.query['limit']);
+          newArr = newArr.slice(0,limit)
+        }
+
+        res.send({
+          "_id":id,
+          "username": user.username,
+          "count": newArr.length,
+          "log": newArr
+        })
+      }
+    }
+
+  } catch(e) {
+    return res.status(400).send(e.message)
+    console.log(e);
+  }
+
+})
 
 
 
-
+//listen
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
